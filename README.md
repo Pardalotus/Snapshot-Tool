edc9febc795fed834cd90d9046fafafe429ccaec
Joe Wass <joe@afandian.com>
2024-12-15 21:25:17 +0000

    feat(count-records): implementation for count-records across Crossref and DataCite snapshots

--- README.md
+++ README.md
@@ -5,3 +5,55 @@ Process metadata snapshots from DataCite and Crossref.
 The Crossref Public Data File is available here: <https://www.crossref.org/learning/public-data-file/>
 
 The DataCite Public Data File is available here: <https://support.datacite.org/docs/datacite-public-data-file>
+
+## Input
+
+Supply the path to a directory with `--input-dir`. This should contain all
+snapshot files you're interested, including Crossref and/or DataCite files. It
+will be scanned recursively, and files with unrecognised extensions will be
+skipped.
+
+The tool expects files with extensions:
+
+ - `*.json.gz` (Crossref)
+ - `*.tgz` (DataCite)
+
+## Installation
+
+Run `cargo build --release`. The binary is available in the `./target/release` directory.
+
+## Functionality
+
+### Show help
+```
+pardalotus_snapshot_tool --help
+```
+
+### Verbose
+
+Add `--verbose` to any command for information on what's going on internally. Useful when reading large mysterious files.
+
+### List files
+```
+pardalotus_snapshot_tool --input-dir /path/to/snapshots
+```
+This is useful for checking which snapshot files will be included.
+
+### Count Records
+```
+pardalotus_snapshot_tool --input-dir /path/to/snapshots --count-records
+```
+
+Count how many metadata records are present across snapshots.
+
+## Limitations
+
+The Crossref zipped JSON files are currently buffered into memory, meaning ~20MB sized allocations. The initial parsing is therefore quite memory-intensive.
+
+When reading a DataCite snapshot typical memory use is 75MB.
+
+When reading the Crossref snapshot the typical memory use is 300 MB.
+
+## License
+
+Copyright 2024 Joe Wass, Pardalotus Technology. This code is Apache 2.0 licensed, see the LICENSE.txt file.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs::{self, File},
+    io::{self, BufRead, Read},
+    path::PathBuf,
+    sync::mpsc::{self, Receiver, Sender},
+    thread,
+};
 
+use serde_json::Value;
 use structopt::StructOpt;
+
+use flate2::read::GzDecoder;
+use tar::Archive;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -19,6 +29,12 @@ struct Options {
         help("Input directory containing snapshot files.")
     )]
     input_dir: Option<PathBuf>,
+
+    #[structopt(long, help("Count all metadata records in snapshot files."))]
+    count_records: bool,
+
+    #[structopt(long, short = "v", help("Send progress messages to STDERR."))]
+    verbose: bool,
 }
 
 fn main() {
@@ -39,11 +55,57 @@ fn main_r() -> anyhow::Result<()> {
     }
 
     if options.list_files {
-        let paths = get_input_files(options)?;
+        let paths = expect_input_files(&options)?;
 
-        for (_, path) in paths {
+        for path in paths {
             if let Some(path_str) = path.to_str() {
                 println!("{}", path_str)
+            }
+        }
+    }
+
+    if options.count_records {
+        let verbose = options.verbose;
+        let paths = expect_input_files(&options)?;
+
+        let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+
+        let read_thread = thread::spawn(move || {
+            if let Err(err) = read_paths_to_channel(&paths, tx, verbose) {
+                eprintln!("Failed read archives: {:?}", err);
+            }
+        });
+
+        // No verbose messages here to avoid crashing thread's messages.
+        let mut count: usize = 0;
+        for _ in rx.iter() {
+            count += 1;
+        }
+        println!("{count}");
+
+        read_thread
+            .join()
+            .unwrap_or_else(|err| eprintln!("Failed to join reader thread: {:?}", err));
+    }
+
+    Ok(())
+}
+
+/// Read all entries in all files to the channel. One entry per message.
+fn read_paths_to_channel(
+    paths: &[PathBuf],
+    tx: Sender<String>,
+    verbose: bool,
+) -> anyhow::Result<()> {
+    for ref path in paths.iter() {
+        // path::ends_with comparison for path doesn't work for sub-path-component chunks.
+        // path::extension only takes the lats extension files so is unsuitbale for `.tar.gz`.
+        if let Some(path_str) = path.to_str() {
+            // Ignore other types.
+            if path_str.ends_with(".tgz") {
+                read_tgz_to_channel(path, &tx, verbose)?;
+            } else if path_str.ends_with(".json.gz") {
+                read_json_gz_to_channel(path, &tx, verbose)?;
             }
         }
     }
@@ -51,14 +113,109 @@ fn main_r() -> anyhow::Result<()> {
     Ok(())
 }
 
-enum FileType {
-    JsonGz,
-    Tgz,
+/// Read a gzipped JSON file.
+/// This is expected to be a Crossref file.
+fn read_json_gz_to_channel(
+    path: &PathBuf,
+    tx: &Sender<String>,
+    verbose: bool,
+) -> anyhow::Result<()> {
+    if verbose {
+        eprintln!("Reading .json.gz {:?}", &path);
+    }
+
+    let f = File::open(path)?;
+    let json = GzDecoder::new(f);
+    let deserialized: Value = serde_json::from_reader(json)?;
+
+    // Crossref files have a top-level key "items" containing items in that snapshot.
+    if let Some(items) = deserialized.get("items").map(|x| x.as_array()).flatten() {
+        let mut count: usize = 0;
+        for item in items {
+            tx.send(item.to_string())?;
+
+            count += 1;
+            if verbose && count % 10000 == 0 {
+                eprintln!("From {:?} read {} lines", path, count);
+            }
+        }
+    } else {
+        eprint!("Didn't get recognised JSON format from {:?}", path);
+    }
+
+    if verbose {
+        eprintln!("Finished reading .json.gz {:?}", &path);
+    }
+
+    Ok(())
 }
 
-fn get_input_files(options: Options) -> anyhow::Result<Vec<(FileType, PathBuf)>> {
-    if let Some(input_dir) = options.input_dir {
-        let files = find_input_files(input_dir)?;
+/// Read all entries in all files in a gzipped tar file to a channel.
+fn read_tgz_to_channel(
+    path: &PathBuf,
+    channel: &Sender<String>,
+    verbose: bool,
+) -> anyhow::Result<()> {
+    let tar_gz = File::open(path)?;
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+
+    if verbose {
+        eprintln!("Read TGZ {:?}", path);
+    }
+
+    for entry in archive.entries()? {
+        let mut ok_entry = entry?;
+        let entry_path = ok_entry.path()?;
+
+        if entry_path
+            .file_name()
+            .map(|x| x.to_str())
+            .flatten()
+            .map(|x| x.ends_with(".jsonl"))
+            .unwrap_or(false)
+        {
+            if verbose {
+                eprintln!("From TGZ {:?} read {:?}", path, entry_path);
+            }
+
+            read_jsonl_to_channel(&mut ok_entry, channel, verbose)?;
+        }
+    }
+
+    if verbose {
+        eprintln!("Finished reading TGZ {:?}", path);
+    }
+
+    Ok(())
+}
+
+/// Read a jsonl (JSON Lines) reader to a channel, one string per line.
+/// These are expected to be found in DataCite snapshots.
+fn read_jsonl_to_channel(
+    reader: &mut dyn Read,
+    channel: &Sender<String>,
+    verbose: bool,
+) -> anyhow::Result<()> {
+    let reader = io::BufReader::new(reader);
+
+    let mut count: usize = 0;
+
+    for line in reader.lines() {
+        channel.send(line?)?;
+        count += 1;
+        if verbose && count % 10000 == 0 {
+            eprintln!("Read {} lines", count);
+        }
+    }
+
+    Ok(())
+}
+
+/// Get a list of input files. Error if no option supplied.
+fn expect_input_files(options: &Options) -> anyhow::Result<Vec<PathBuf>> {
+    if let Some(ref input_dir) = options.input_dir {
+        let files = find_input_files(&input_dir)?;
         return Ok(files);
     } else {
         return Err(anyhow::format_err!("Please supply <input-files>"));
@@ -66,25 +223,24 @@ fn get_input_files(options: Options) -> anyhow::Result<Vec<(FileType, PathBuf)>>
 }
 
 /// Return list of relevant files from directory.
-fn find_input_files(input_dir: std::path::PathBuf) -> anyhow::Result<Vec<(FileType, PathBuf)>> {
-    let mut paths: Vec<(FileType, PathBuf)> = vec![];
+fn find_input_files(input_dir: &std::path::PathBuf) -> anyhow::Result<Vec<PathBuf>> {
+    let mut paths: Vec<PathBuf> = vec![];
 
-    fn r(
-        input_dir: std::path::PathBuf,
-        paths: &mut Vec<(FileType, PathBuf)>,
-    ) -> anyhow::Result<()> {
+    fn r(input_dir: &std::path::PathBuf, paths: &mut Vec<PathBuf>) -> anyhow::Result<()> {
         for entry in fs::read_dir(input_dir)? {
             let path = entry?.path();
             if path.is_file() {
                 if let Some(path_str) = path.to_str() {
-                    if path_str.ends_with(".json.gz") {
-                        paths.push((FileType::JsonGz, path));
-                    } else if path_str.ends_with(".tgz") {
-                        paths.push((FileType::Tgz, path));
+                    // Crossref public data file torrent is many `.json.gz` files.
+                    if path_str.ends_with(".json.gz") ||
+                    // DataCite public data file is one `.tgz` file with many `.jsonl` entries.
+                    path_str.ends_with(".tgz")
+                    {
+                        paths.push(path);
                     }
                 }
             } else if path.is_dir() {
-                r(path, paths)?;
+                r(&path, paths)?;
             }
         }
 
